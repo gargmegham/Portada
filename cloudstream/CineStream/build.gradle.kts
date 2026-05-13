81b63d66ff45f035569e16d7f11cd3e4a12ba797
Megham Garg <gargmegham3@gmail.com>
2026-05-13 15:39:29 +0530

    cloudstream extensions cleanup

---
+++ cloudstream/CineStream/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 431
+android {
+    defaultConfig {
+        val properties = Properties()
+        val localPropsFile = project.rootProject.file("local.properties")
+        if (localPropsFile.exists()) {
+            localPropsFile.inputStream().use { properties.load(it) }
+        }
+        android.buildFeatures.buildConfig=true
+        buildConfigField("String", "SIMKL_API", "\"${properties.getProperty("SIMKL_API", "")}\"")
+        buildConfigField("String", "TMDB_KEY", "\"${properties.getProperty("TMDB_KEY", "")}\"")
+        buildConfigField("String", "CC_COOKIE", "\"${properties.getProperty("CC_COOKIE", "")}\"")
+        buildConfigField("String", "CINE_API", "\"${properties.getProperty("CINE_API", "")}\"")
+        buildConfigField("String", "CASTLE_API", "\"${properties.getProperty("CASTLE_API", "")}\"")
+    }
+}
+
+cloudstream {
+    // language = "en"
+    description = "One stop solution for Movies, Series, Anime, AsianDrama and Torrents"
+    authors = listOf("megix")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime",
+        "Torrent"
+    )
+
+    iconUrl = "https://github.com/SaurabhKaperwan/CSX/raw/refs/heads/master/CineStream/icon.png"
+}
